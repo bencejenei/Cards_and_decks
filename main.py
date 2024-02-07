32d2bca5859684c25a5249220c4a55d171168e71
bencejenei <bencejenei94@gmail.com>
2024-02-07 21:43:07 +0900

    Add PlayingCard dataclass

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PlayingCard:
+    rank: str
+    suit: str
 
 
 if __name__ == '__main__':
-    print("hello_world")
+    queen_of_hearts = PlayingCard('Q', 'Hearts')
+    print(queen_of_hearts.rank)
